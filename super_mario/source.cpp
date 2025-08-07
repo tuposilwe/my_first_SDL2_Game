b7bbf2f81986b90c40958eee4451182289a6adf4
tuposilwe <rudigermkondya@gmail.com>
2025-08-07 08:46:34 +0300

    delaying window display

--- super_mario/source.cpp
+++ super_mario/source.cpp
@@ -2,7 +2,22 @@
 
 int main(int argc, char* argv[])
 {
+	// sdl creation
 	SDL_Init(SDL_INIT_VIDEO);
+
+	// window creation
+
+	SDL_Window* window = SDL_CreateWindow(
+		"SDL Window",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		640,
+		480,
+		SDL_WINDOW_SHOWN
+	);
+
+	// delaying window display
+	SDL_Delay(3000);
 
 	SDL_Quit();
 
